bca14e34137149aedb2d883e2e82075bc9177e55
Dennis-webdev <dennisrichter.business@gmail.com>
2020-05-03 15:11:33 +0200

    Updates

---
+++ precache-manifest.bc0a1468bdcf921107ed88cab1899861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29baa578ce8f8f81431e7b0be60598b",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "7012e53b539e641744f2",
+    "url": "/TicTacToe/static/css/main.b78ea2f2.chunk.css"
+  },
+  {
+    "revision": "a21d82df8e6e19a064d6",
+    "url": "/TicTacToe/static/js/2.6873c516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.6873c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7012e53b539e641744f2",
+    "url": "/TicTacToe/static/js/main.df6ae847.chunk.js"
+  },
+  {
+    "revision": "21520be500339c774d7f",
+    "url": "/TicTacToe/static/js/runtime-main.c92e2dbe.js"
+  }
+]);
